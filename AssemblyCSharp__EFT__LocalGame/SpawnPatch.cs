0e420a05413d0166f66ceb70adeacf780e874fc2
ragnaroks <703821945@qq.com>
2025-05-25 06:50:45 +0800

    misc

--- AssemblyCSharp__EFT__LocalGame/SpawnPatch.cs
+++ AssemblyCSharp__EFT__LocalGame/SpawnPatch.cs
@@ -2,15 +2,14 @@
 using System.Reflection;
 
 namespace SPTExfilNow.AssemblyCSharp__EFT__LocalGame {
-    public  class SpawnPatch : ModulePatch {
+    public class SpawnPatch : ModulePatch {
         protected override MethodBase GetTargetMethod () {
-            return typeof(EFT.LocalGame).GetMethod(nameof(EFT.LocalGame.Spawn),BindingFlags.Public|BindingFlags.Instance);
+            return typeof(EFT.LocalGame).GetMethod(nameof(EFT.LocalGame.Spawn), BindingFlags.Public | BindingFlags.Instance);
         }
 
         [PatchPostfix]
         private static void Postfix (ref EFT.LocalGame __instance) {
             SPTExfilNowPlugin.LocalGame = __instance;
-            
         }
     }
 }
--- SPTExfilNowPlugin.cs
+++ SPTExfilNowPlugin.cs
@@ -10,19 +10,19 @@ using UnityEngine;
 namespace SPTExfilNow {
     [BepInPlugin("net.skydust.SPTExfilNowPlugin", "SPTExfilNowPlugin", "1.0.3")]
     [BepInProcess("EscapeFromTarkov")]
-    public class SPTExfilNowPlugin : BaseUnityPlugin {        
-        private Boolean IsBusy{get;set;} = false;
-                
-        private AssemblyCSharp__EFT__LocalGame.SpawnPatch? SpawnPatch{get;set;} = null;
-        
-        public static ConfigEntry<KeyboardShortcut>? ExfilShortcutKey{get;private set;} =null;
+    public class SPTExfilNowPlugin : BaseUnityPlugin {
+        private Boolean IsBusy { get; set; } = false;
+
+        private AssemblyCSharp__EFT__LocalGame.SpawnPatch? SpawnPatch { get; set; } = null;
+
+        public static ConfigEntry<KeyboardShortcut>? ExfilShortcutKey { get; private set; } = null;
 
         //public static ConfigEntry<Boolean>? EnableESP{get;private set;} = null;
 
-        public static EFT.LocalGame? LocalGame{get;set;} = null;
+        public static EFT.LocalGame? LocalGame { get; set; } = null;
 
         protected void Awake () {
-            SPTExfilNowPlugin.ExfilShortcutKey = this.Config.Bind<KeyboardShortcut>("config","exfil-shortcut",new KeyboardShortcut(KeyCode.Backslash,KeyCode.LeftControl),"shortcut for exfil now, default is [CTRL + \\]");
+            SPTExfilNowPlugin.ExfilShortcutKey = this.Config.Bind<KeyboardShortcut>("config", "exfil-shortcut", new KeyboardShortcut(KeyCode.Backslash, KeyCode.LeftControl), "shortcut for exfil now, default is [CTRL + \\]");
             //SPTExfilNowPlugin.EnableESP = this.Config.Bind<Boolean>("config","enable-ESP",false,"exfil point ESP");
             this.SpawnPatch = new AssemblyCSharp__EFT__LocalGame.SpawnPatch();
             this.Logger.LogDebug("plugin loaded");
@@ -34,25 +34,26 @@ namespace SPTExfilNow {
         }
 
         protected void Update () {
-            if(this.IsBusy){return;}
-            if(SPTExfilNowPlugin.ExfilShortcutKey?.Value.IsUp()!=true) {return;}
-            if(SPTExfilNowPlugin.LocalGame?.GameType!=EGameType.Offline){return;}
+            if (this.IsBusy) { return; }
+            if (SPTExfilNowPlugin.ExfilShortcutKey?.Value.IsUp() != true) { return; }
+            if (SPTExfilNowPlugin.LocalGame?.GameType != EGameType.Offline) { return; }
             this.IsBusy = true;
             this.ExfilNow();
         }
 
-        protected void OnDestroy() {
+        protected void OnDestroy () {
             this.SpawnPatch?.Disable();
+            SPTExfilNowPlugin.LocalGame = null;
             this.Logger.LogDebug("plugin deactived");
         }
 
         private void ExfilNow () {
             GameWorld? gameWorld = Singleton<GameWorld>.Instance;
-            if(gameWorld==null){
+            if (gameWorld == null) {
                 this.IsBusy = false;
                 return;
             }
-            if (SPTExfilNowPlugin.LocalGame?.GameType!=EGameType.Offline) {
+            if (SPTExfilNowPlugin.LocalGame?.GameType != EGameType.Offline) {
                 this.IsBusy = false;
                 return;
             }
@@ -64,25 +65,26 @@ namespace SPTExfilNow {
             ExfiltrationPoint? exfiltrationPoint = null;
             switch (gameWorld.MainPlayer.Fraction) {
                 case ETagStatus.Scav:
-                    exfiltrationPoint = exfiltrationController.ScavExfiltrationPoints.FirstOrDefault(x=>x.isActiveAndEnabled && !x.HasRequirements);
+                exfiltrationPoint = exfiltrationController.ScavExfiltrationPoints.FirstOrDefault(x => x.isActiveAndEnabled && !x.HasRequirements);
                 break;
                 case ETagStatus.Usec:
                 case ETagStatus.Bear:
-                    exfiltrationPoint = exfiltrationController.ExfiltrationPoints.FirstOrDefault(x=>x.isActiveAndEnabled && !x.HasRequirements);
+                exfiltrationPoint = exfiltrationController.ExfiltrationPoints.FirstOrDefault(x => x.isActiveAndEnabled && !x.HasRequirements);
                 break;
-                default:break;
-            }            
-            if(exfiltrationPoint==null){
+                default:
+                break;
+            }
+            if (exfiltrationPoint == null) {
                 this.IsBusy = false;
                 NotificationManagerClass.DisplayMessageNotification("not found any available exfil point");
                 return;
             }
             String exfilName = exfiltrationPoint.Settings.Name.Localized();
-            SPTExfilNowPlugin.LocalGame.Stop(gameWorld.MainPlayer.ProfileId, ExitStatus.Survived,exfilName);
+            SPTExfilNowPlugin.LocalGame.Stop(gameWorld.MainPlayer.ProfileId, ExitStatus.Survived, exfilName);
             SPTExfilNowPlugin.LocalGame = null;
             this.IsBusy = false;
-            NotificationManagerClass.DisplayMessageNotification(String.Concat("exfil at ",exfilName));
-            this.Logger.LogDebug(String.Concat("exfil at ",exfilName));
+            NotificationManagerClass.DisplayMessageNotification(String.Concat("exfil at ", exfilName));
+            this.Logger.LogDebug(String.Concat("exfil at ", exfilName));
         }
     }
 }
